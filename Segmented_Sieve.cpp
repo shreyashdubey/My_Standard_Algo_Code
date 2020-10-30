ed9ad28c1c1b250cdc5137545c5106453b1c21c3
shreyashdubey <43183170+shreyashdubey@users.noreply.github.com>
2020-10-30 22:26:26 +0530

    Add files via upload

---
+++ Segmented_Sieve.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+vector<bool> segmentedSieve(long long L, long long R) {
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<bool> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<bool> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+int main()
+{
+	ll l,r; cin>>l>>r;
+	vector<bool> v=segmentedSieve(l,r);
+	//for(auto i:v) cout<<i<<" ";
+		for(int i=0;i<(int)v.size();i++)
+		{
+			if(v[i])
+			{
+				cout<<i+l<<" ";
+			}
+		}
+}
